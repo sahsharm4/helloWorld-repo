52f4f9d3d7ae9e32845c23f17071a075d2d6524e
Saharsh Sharma <sahsharm4@publicisgroupe.net>
2022-06-15 14:33:06 +0530

    ddf

---
+++ junit5-project/src/main/java/com/sapient/asde/service/Person.java
@@ -0,0 +1,17 @@
+package com.sapient.asde.service;
+
+import java.time.LocalDate;
+/*
+        Create tester class for Person testing getter, setter and constructor methods
+        Code coverage:  >90%
+ */
+public class Person {
+    private Long adharCard;
+    private String name;
+    private LocalDate birthdate;
+    private String address;
+    private Long mobile;
+
+
+    //TO DO
+}
